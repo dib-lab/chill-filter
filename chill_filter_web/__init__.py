ee874467ac037aae08a0079cb5e9f3954adb63e5
C. Titus Brown <titus@idyll.org>
2024-12-15 17:31:01 -0800

    clean up abund etc template

--- chill_filter_web/__init__.py
+++ chill_filter_web/__init__.py
@@ -19,9 +19,10 @@ KSIZE = 51
 SCALED = 100_000
 UPLOAD_FOLDER = "/tmp/chill"
 EXAMPLES_DIR = os.path.join(os.path.dirname(__file__), "../examples/")
+SIGNATURES = "prepare-db/plants+animals+gtdb.mf.csv"
+DATABASE = "prepare-db/plants+animals+gtdb.rocksdb"
 
 app = Flask(__name__)
-
 
 def load_sig(fullpath):
     try:
@@ -34,6 +35,75 @@ def load_sig(fullpath):
         pass
 
     return None
+
+
+def run_gather(outpath, csv_filename):
+    start = time.time()
+    status = branch.do_fastmultigather(
+        outpath,
+        DATABASE,
+        0,
+        KSIZE,
+        SCALED,
+        MOLTYPE,
+        csv_filename,
+        False,
+        False,
+    )
+    end = time.time()
+
+    print(f"branchwater gather status: {status}; time: {end - start:.2f}s")
+    return status
+
+
+def sig_is_assembly(ss):
+    mh = ss.minhash
+    # track abundance set? => assembly
+    if not mh.track_abundance:
+        print('ZZZ1 - is assembly')
+        return True
+
+    # count the number > 1 in abundance
+    n_above_1 = sum(1 for (hv, ha) in mh.hashes.items() if ha > 1)
+    print('ZZZ2', n_above_1, len(mh), n_above_1/len(mh))
+
+    # more than 10% > 1? => probably not assemblyy
+    if n_above_1 / len(mh) > 0.1:
+        return False
+
+    # nope! assembly!
+    return True
+
+
+merged_hashes = None
+def build_merged_sig():
+    global merged_hashes
+    if merged_hashes is None:
+        ## build a merged sig - CTB hackity hack
+        print('building merged sig from signatures...')
+        idx = sourmash.load_file_as_index(SIGNATURES)
+        merged_mh = None
+        for ss in idx.signatures():
+            if merged_mh is None:
+                merged_mh = ss.minhash.copy_and_clear().flatten().to_mutable()
+            else:
+                merged_mh += ss.minhash.flatten()
+        merged_hashes = merged_mh.hashes
+        print('...done!')
+    return merged_hashes
+
+def estimate_weight_of_unknown(ss, *, CUTOFF=5):
+    merged_hashes = build_merged_sig()
+    mh = ss.minhash
+
+    print(len(mh))
+
+    unknown = [ (hv, ha) for (hv, ha) in mh.hashes.items() if ha not in merged_hashes ]
+    sum_unknown = sum( ha for (hv, ha) in unknown )
+    sum_high = sum( ha for (hv, ha) in unknown if ha >= CUTOFF )
+    sum_low = sum( ha for (hv, ha) in unknown if ha < CUTOFF )
+
+    return sum_high / sum_unknown, sum_low / sum_unknown
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -127,27 +197,15 @@ def get_md5(path):
             success = True
 
     if success:
-        # @CTB check that it's weighted?
         assert ss is not None
         sample_name = ss.name or "(unnamed sample)"
-        if action == "search":
+        if action == 'download_csv':
+            csv_filename = filename + ".gather.csv"
+            return send_from_directory(UPLOAD_FOLDER, csv_filename)
+        elif action == "search":
             csv_filename = outpath + ".gather.csv"
             if not os.path.exists(csv_filename):
-                start = time.time()
-                status = branch.do_fastmultigather(
-                    outpath,
-                    "prepare-db/plants+animals+gtdb.rocksdb",
-                    0,
-                    KSIZE,
-                    SCALED,
-                    MOLTYPE,
-                    csv_filename,
-                    False,
-                    False,
-                )
-                end = time.time()
-
-                print(f"branchwater gather status: {status}; time: {end - start:.2f}s")
+                status = run_gather(outpath, csv_filename)
                 if status != 0:
                     return "search failed, for reasons that are probably not your fault"
                 else:
@@ -155,25 +213,50 @@ def get_md5(path):
             else:
                 print(f"using cached output in: '{csv_filename}'")
 
-            # load/process
             gather_df = pd.read_csv(csv_filename)
-            gather_df = gather_df[gather_df["f_unique_weighted"] >= 0.001]
-            if len(gather_df):
-                last_row = gather_df.tail(1).squeeze()
-                sum_weighted_found = last_row["sum_weighted_found"]
-                total_weighted_hashes = last_row["total_weighted_hashes"]
 
-                f_found = sum_weighted_found / total_weighted_hashes
+            # process abundance-weighted matches
+            if not sig_is_assembly(ss):
+                f_unknown_high, f_unknown_low = estimate_weight_of_unknown(ss)
+                print('YYY', f_unknown_high, f_unknown_low)
 
-                return render_template(
-                    "sample_search.html",
-                    sample_name=sample_name,
-                    sig=ss,
-                    gather_df=gather_df,
-                    f_found=f_found,
-                )
+                gather_df = gather_df[gather_df["f_unique_weighted"] >= 0.001]
+                if len(gather_df):
+                    last_row = gather_df.tail(1).squeeze()
+                    sum_weighted_found = last_row["sum_weighted_found"]
+                    total_weighted_hashes = last_row["total_weighted_hashes"]
+                    
+
+                    f_found = sum_weighted_found / total_weighted_hashes
+
+                    return render_template(
+                        "sample_search_abund.html",
+                        sample_name=sample_name,
+                        sig=ss,
+                        gather_df=gather_df,
+                        f_found=f_found,
+                        f_unknown_high=f_unknown_high,
+                        f_unknown_low=f_unknown_low,
+                    )
+                else:
+                    return "no matches found!"
+            # process flat matching (assembly)
             else:
-                return "no matches found!"
+                print('running flat')
+                gather_df = gather_df[gather_df["f_unique_weighted"] >= 0.001]
+                if len(gather_df):
+                    last_row = gather_df.tail(1).squeeze()
+                    f_found = gather_df['f_unique_to_query'].sum()
+
+                    return render_template(
+                        "sample_search_flat.html",
+                        sample_name=sample_name,
+                        sig=ss,
+                        gather_df=gather_df,
+                        f_found=f_found,
+                    )
+                else:
+                    return "no matches found!"
 
         elif action == "download":
             return send_from_directory(UPLOAD_FOLDER, filename)
