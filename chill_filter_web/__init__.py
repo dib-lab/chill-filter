167dff2845a7ee3ea4a6707aabd9654949beb4d6
C. Titus Brown <titus@idyll.org>
2024-12-17 12:58:54 -0800

    enrich display

--- chill_filter_web/__init__.py
+++ chill_filter_web/__init__.py
@@ -33,6 +33,29 @@ def percent(value):
     return f"{value*100:.1f}%"
 jinja_env.filters['percent'] = percent
 
+def format_bp(bp):
+    "Pretty-print bp information."
+    bp = float(bp)
+    if bp < 500:
+        return f"{bp:.0f} bp"
+    elif bp <= 500e3:
+        return f"{round(bp / 1e3, 1):.1f} kbp"
+    elif bp < 500e6:
+        return f"{round(bp / 1e6, 1):.1f} Mbp"
+    elif bp < 500e9:
+        return f"{round(bp / 1e9, 1):.1f} Gbp"
+    return f"??? {bp}"
+jinja_env.filters['format_bp'] = format_bp
+
+def unique_weighted_bp(item):
+    return format_bp(item['n_unique_weighted_found'] * item['scaled'])
+jinja_env.filters['unique_weighted_bp'] = unique_weighted_bp
+
+def unique_flat_bp(item):
+    return format_bp(item['unique_intersect_bp'])
+jinja_env.filters['unique_flat_bp'] = unique_flat_bp
+
+
 def load_sig(fullpath):
     try:
         ss = sourmash.load_file_as_index(fullpath)
