3ec4bf7e489bc82caa5785f3af5db73ffc4f60bc
C. Titus Brown <titus@idyll.org>
2024-12-25 08:06:03 -0800

    use conf

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,7 +1,7 @@
 # /path-to-your-project/gunicorn_conf.py
 bind = '0.0.0.0:5000'
 worker_class = 'sync'
-loglevel = 'debug'
+#loglevel = 'debug'
 accesslog = 'logs/access_log'
 acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
 errorlog =  'logs/error_log'
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 source ~/miniforge3/etc/profile.d/conda.sh
 conda activate chill
 mkdir -p logs
-gunicorn -w 4 -b localhost:5000 chill_filter_web:app
+gunicorn -w 4 -b localhost:5000 -c gunicorn_conf.py chill_filter_web:app 
